1d99e507f1c80bc507a33f58a9099cc10f5a8cc2
Andrew Huynh <huynhandrewv@gmail.com>
2018-10-13 09:56:07 -0400

    intro to hello world and comments

---
+++ comments.py
@@ -0,0 +1,9 @@
+# This is a comment
+# Comments are used so that you can write notes
+# Using the # symbol will tell python to ignore the rest of this line
+print("Hello!") # this comment is for this line
+
+# If you put the # between quotes, python will NOT ignore it
+print("# this is not a comment")
+
+# What else do you think comments might be good for?
